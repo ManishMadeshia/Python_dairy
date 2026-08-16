8be5c7ee35891d67cf420e2b927a340aac2d15b2
Manish Madeshia <manishmadeshia1@gmail.com>
2026-08-16 16:44:59 +0530

    solve question on syntax, varibale, datatpe, basic operator

--- Day5_question_prep/solution.py
+++ Day5_question_prep/solution.py
@@ -40,7 +40,7 @@ for i in range(1,num+1):
 
 print(factorial)
 
-'''
+
 
 #Q6. create a varibale of different data type : int, float, string, and boolean . print their value and type
 
@@ -91,4 +91,118 @@ str2 = input("Enter second string")
 
 result = str1 +" " + str2
 
-print(result)
+print(result) 
+
+#Q10. write a python program to check if a variable is of specific datatype 
+
+a = 7
+
+if type(a) == int:
+    print("integer datatpe")
+elif type(a) == float:
+    print("float datatype")
+elif type(a) == str:
+    print("String datatype")
+else :
+    ("not in list of int, float, string")
+
+
+'''
+
+
+
+#------------------arithmetic operator----------------
+#11. write a python program to perform addition,subtraction,multiplication,division.
+
+a = 10
+b = 33
+
+print(a+b) #addition
+print(a-b)  #subtraction
+print(a*b)  #multiplication
+print(a/b) #division
+print(a//b) #floor division
+print(a%b)  #modulos operator
+
+
+#12. write a python progem to demonstrate comparsion operator equal to, not equal to , greater than, less than ,
+
+a = 12
+b = 33
+
+#greater than
+if a>b:
+    print("a is greater")
+else : 
+    print('b is greater')
+
+#equal to 
+if a==b :
+    print("a &b are equal")
+else:
+    print("not equal")
+
+# less than operator
+if a < b :
+    print("a is greater")
+
+#not equal operator
+if a != b:
+    print("not equal")
+
+
+#13. write a python program to demonstrate logical operator : and , or , not
+
+age = 21
+salary = 20000
+is_eliglible = True
+
+if age >= 18 and salary >= 30000:
+    print("Eligible for loan")
+else:
+    print("Not eligible")
+
+#--------or-----------
+
+if age >= 18 or salary >=30000:
+    print("Partial eliglible! Need to talk with management")
+
+#-----------not-----
+
+if not is_eliglible :
+    print("not eliglible")
+else:
+    print("Eliglible")
+
+#14. program to calculate the square of a number
+
+num = int(input("Enter a no: "))
+
+squ = num * num
+print(f"the square of num is : {squ}")
+
+#15. program to check if a num is odd or even
+
+num = int(input("Enter a num: "))
+
+if num <=0:
+    print("please enter num greater than 0")
+else:
+    if num%2 == 0:
+        print("even")
+    else:
+        print("Odd")
+
+#Q16. program to find sum od first n natural no
+
+n = int(input("enter a no: "))
+
+sum = 0
+
+for i in range(1,n+1):
+    sum = sum +i
+print(sum)
+
+#Q17.program to check leap year
+
+
