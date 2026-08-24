08966c4de617fd3862b9b548cfc5772492d4a849
Manish Madeshia <manishmadeshia1@gmail.com>
2026-08-24 12:08:36 +0530

    list fuction

---
+++ day7_List/list_function.py
@@ -0,0 +1,32 @@
+#Find length of list
+li = [1,2,3,5,6]
+print(len(li))
+
+#Find maximum number.
+print(max(li))
+
+#Find minimum number.
+print(min(li))
+
+#Find sum of numbers.
+print(sum(li))
+
+#Find average.
+print(sum(li)/len(li))
+
+#Find length of fruits list.
+fruits = ['grapes','orange','chikko','kiwi']
+print(len(fruits))
+
+#Find highest mark.
+marks = [66,77,88,66,99]
+print(max(marks))
+
+#Find lowest mark.
+print(min(marks))
+
+#Find total salary.
+print(sum(marks))
+
+#Find average marks.
+print(sum(marks)/len(marks))
